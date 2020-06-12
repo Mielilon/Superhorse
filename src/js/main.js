3b36e951157e9e729f6a1ec8b9eabe811b3b0df4
Alexander Gusev <san4es021@gmail.com>
2020-06-12 19:17:51 +0300

    Добавлена логика JS

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,39 @@
-//= jquery.min.js
+//= vue.js
+//= productsData.js
+
+const $ = document.querySelector.bind(document);
+
+const app = new Vue({
+  el: '#app',
+  data: () => ({
+    productsData,
+    isProductModalActive: false,
+    selectedProductId: null,
+    activeTab: 0,
+  }),
+  mounted() {
+    const catalogue = $('.catalogue__goods');
+    if (catalogue) catalogue.style.visibility = 'visible';
+  },
+  computed: {
+    activeProduct() {
+      return this.productsData[this.selectedProductId];
+    },
+    getCurrentYear() {
+      return new Date().getFullYear();
+    },
+  },
+  methods: {
+    showProductModal(id) {
+      this.isProductModalActive = true;
+      this.selectedProductId = id;
+    },
+    closeProductModal() {
+      this.isProductModalActive = false;
+      this.activeTab = 0;
+    },
+    selectTab(id) {
+      this.activeTab = id;
+    },
+  },
+});
