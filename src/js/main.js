2e1aa37d3c3397a32f822a21db46432e3faef9b8
Alexander Gusev <san4es021@gmail.com>
2020-06-13 15:13:00 +0300

    Форма отправки предварительного заказа

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@
 //= productsData.js
 //= regionsList.js
 //= popper.min.js
+//= axios.min.js
 
 const $ = document.querySelector.bind(document);
 
@@ -13,6 +14,30 @@ const app = new Vue({
     selectedProductId: null,
     activeTab: 0,
     regionsList,
+    contactForm: {
+      name: {
+        value: '',
+        alert: '',
+        required: true,
+      },
+      email: {
+        value: '',
+        alert: '',
+        required: false,
+        regExp: /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/i,
+      },
+      phone: {
+        value: '',
+        alert: '',
+        required: true,
+      },
+      message: {
+        value: '',
+        alert: '',
+        required: false,
+      },
+    },
+    contactFormResult: '',
   }),
   mounted() {
     const catalogue = $('.catalogue__goods');
@@ -27,6 +52,56 @@ const app = new Vue({
     },
   },
   methods: {
+    prepareMailAndSend(formName = 'contactForm') {
+      let essentialMsg = 'Поле обязятельно для заполнения';
+      let incorrectMsg = 'Поле заполнено некорректно';
+      let error = false;
+
+      for (let i in this[formName]) {
+        let field = this[formName][i];
+
+        if (field.required && field.value.length < 1) {
+          field.alert = essentialMsg;
+          error = true;
+          continue;
+        } else {
+          field.alert = '';
+        }
+
+        if (field.regExp && !field.regExp.test(field.value)) {
+          field.alert = incorrectMsg;
+          error = true;
+          continue;
+        } else {
+          field.alert = '';
+        }
+      }
+
+      if (!error) this.sendMail(formName);
+    },
+    sendMail(formName = 'contactForm') {
+      let bodyFormData = new FormData();
+      bodyFormData.set('name', this[formName].name.value);
+      bodyFormData.set('email', this[formName].email.value);
+      bodyFormData.set('phone', this[formName].phone.value);
+      bodyFormData.set('message', this[formName].message.value);
+
+      this[formName + 'Result'] = 'Отправляется...';
+
+      axios({
+        method: 'post',
+        url: '/contacts.php',
+        data: bodyFormData,
+        config: { headers: { 'Content-Type': 'multipart/form-data' } },
+      })
+        .then((res) => {
+          this[formName + 'Result'] = 'Сообщение успешно отправлено';
+        })
+        .catch((err) => {
+          this[formName + 'Result'] =
+            'При отправке сообщения возникла ошибка. Свяжитесь с нами по почте gigienakrs@yandex.ru';
+        });
+    },
     showProductModal(id) {
       this.isProductModalActive = true;
       this.selectedProductId = id;
